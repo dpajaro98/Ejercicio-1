3297566920d35f3aaae9bba2038a3b981412089e
dpajaro98 <drpajaro7@misena.edu.co>
2022-08-01 21:17:57 -0500

    Ejercicios de practica

---
+++ ListaEjercicios.js
@@ -0,0 +1,21 @@
+const ListaPrueba=[
+    "Darien Rafael Pajaro Miranda",
+    24,
+    true,
+    2022-10-08      
+]
+
+const ObjetoLibroFavorito={
+    Titulo:"Mi Viaje Sin Ti",
+    Autor:"Alejandro Sequera",
+    fecha:new Date(2017,11,12),
+    url:"https://g.co/kgs/Rt5gVD"
+
+}
+
+console.log(ObjetoLibroFavorito)
+console.log(ListaPrueba)
+
+//
+//- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+
