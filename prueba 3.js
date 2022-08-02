731fca0a4d7056453e2457094219c8af0023b767
dpajaro98 <drpajaro7@misena.edu.co>
2022-08-01 21:19:20 -0500

    cambios

--- prueba 3.js
+++
@@ -1,8 +0,0 @@
-const ahora=new Date()
-console.log(ahora)
-const Fecha_prueba=new Date(2022,11,15)
-console.log(Fecha_prueba)
-const Dia_prueba=Fecha_prueba.getDate()
-const mes_prueba=Fecha_prueba.getMonth()
-const anio_prueba=Fecha_prueba.getFullYear()
-console.log(Dia_prueba+"/",mes_prueba+"/",anio_prueba)
